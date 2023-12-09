d49abf82f02350baecb944cea9b855ea913fc4bd
NEM-ICU <itt1819025@tec.rjt.ac.lk>
2023-12-09 12:28:57 -0500

    initial setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,14 @@
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config({ path: "./config.env" });
+import { app } from "./app.js";
+import dbConnect from "./config/db.js";
 
-console.log(process.env.MODE);
+dbConnect();
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
